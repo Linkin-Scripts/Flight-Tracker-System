543190aacfff3b4d5134919504c95a27ff95af95
daniel-morg-silva <dms121970@gmail.com>
2026-04-22 16:35:57 +0200

    fix: removed obj and bin folders

--- Flight Tracker System/obj/Debug/net10.0/Flight Tracker System.AssemblyInfo.cs
+++ Flight Tracker System/obj/Debug/net10.0/Flight Tracker System.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Flight Tracker System")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+373ecc1fc4f56375289fe6b1c94211f5fa53af1d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e933a7a96ebec1e4ffd947bc4c04399e4a8d538c")]
 [assembly: System.Reflection.AssemblyProductAttribute("Flight Tracker System")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Flight Tracker System")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
